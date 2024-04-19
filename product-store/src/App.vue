3abb93054d475f5373d076aea68a0ce21d233876
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-04-19 08:54:55 -0300

    feat(App.vue): Implemented function editproduct using endpoint PUT

--- product-store/src/App.vue
+++ product-store/src/App.vue
@@ -9,9 +9,9 @@
       :key="index"
       >
       <li>
-        Id: {{ product.id }} Produto: {{ product.name }} 
-        <button @click="deletarProduto(product.id)" > X </button>
-        <button @click="inativarProduto(product.id)">Inativar</button>
+        Id: {{ product.id }} <br> Produto: {{ product.name }} <br> Preço: {{ product.price }} <br> Descrição: {{ product.description }} <br>
+        <Button @click="buscarProduto(product.id)">Editar</Button>
+        <Button @click="inativarProduto(product.id)">Inativar</Button>
       </li>  
     </ul>
   </div>
@@ -23,8 +23,8 @@
       >
       <li>
         Id: {{ product.id }} Produto: {{ product.name }} 
-        <button @click="deletarProduto(product.id)" > X </button>
-        <button @click="ativarProduto(product.id)">Ativar</button>
+        <Button @click="deletarProduto(product.id)" > Deletar </Button>
+        <Button @click="ativarProduto(product.id)">Ativar</Button>
       </li>  
     </ul>
   </div>
@@ -67,6 +67,44 @@
                 <Button type="button" label="Save" @click="adicionarProduto(this.product)"></Button>
             </div>
   </Dialog>
+  <Dialog 
+          modal header="Editar Produto" 
+          v-model:visible="visibleEdit" 
+          :style="{ width: '50rem' }"
+        >
+            <span class="p-text-secondary block mb-5">Insira os novos dados do produto</span>
+            <div class="flex align-items-center gap-3 mb-3">
+                <label for="name" class="font-semibold w-6rem">Nome</label>
+                <InputText 
+                  autocomplete="off"
+                  class="flex-auto" 
+                  id="name"
+                  v-model="product.name" 
+                />
+            </div>
+            <div class="flex align-items-center gap-3 mb-5">
+                <label for="price" class="font-semibold w-6rem">Preço</label>
+                <InputText 
+                  autocomplete="off" 
+                  class="flex-auto" 
+                  id="price"
+                  v-model="product.price" 
+                />
+            </div>
+            <div class="flex align-items-center gap-3 mb-5">
+                <label for="description" class="font-semibold w-6rem">Descrição</label>
+                <InputText 
+                  autocomplete="off" 
+                  class="flex-auto" 
+                  id="description"
+                  v-model="product.description" 
+                />
+            </div>
+            <div class="flex justify-content-end gap-2">
+                <Button type="button" label="Cancel" severity="secondary" @click="visibleEdit = false"></Button>
+                <Button type="button" label="Save" @click="editarProduto(product.id, this.product)"></Button>
+            </div>
+        </Dialog>
   <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -75,6 +113,7 @@
 </template>
 
 <script>
+import Button from 'primevue/button';
 import api from './plugins/axios';
 
   export default {
@@ -82,9 +121,11 @@ import api from './plugins/axios';
     data() {
       return {
         visible: false,
+        visibleEdit: false,
         productsActives: [],
         productsInactives: [],
         product: {
+          id: '',
           name: '',
           price: '',
           description: ''
@@ -101,21 +142,43 @@ import api from './plugins/axios';
         }
       },
       mostraProdutos() {
-          api.get('/products?isActive=true')
-          .then(resp => {
-             this.productsActives = resp.data.data
-          })
-          .catch(error => {
+        api.get('/products?isActive=true')
+        .then(resp => {
+            this.productsActives = resp.data.data
+        })
+        .catch(error => {
+          console.log(error);
+        });
+        api.get('/products?isActive=false')
+        .then(resp => {
+            this.productsInactives = resp.data.data
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      },
+      buscarProduto(id){
+
+        api.get(`/products/${id}`)
+        .then(resp => {
+          this.product.id =  resp.data.data.id
+          this.product.name = resp.data.data.name
+          this.product.price = resp.data.data.price
+          this.product.description = resp.data.data.description
+          this.visibleEdit = true
+        })
+      },
+      editarProduto(id, product){
+        api.put(`/products/${id}`, product)
+        .then(resp => {
+            this.visibleEdit = false
+            this.mostraProdutos()
+            this.limparCadastro()
+        })
+        .catch(error => {
             console.log(error);
-          });
-          api.get('/products?isActive=false')
-          .then(resp => {
-             this.productsInactives = resp.data.data
-          })
-          .catch(error => {
-            console.log(error);
-          });
-        },
+        });
+      },
       adicionarProduto(product){
         api.post('/products/', product)
           .then(resp => {
